34cc33b328dc2f7d394aaf29e07f989df4851c8d
VadymNadv <nadvynychnyy12345@gmail.com>
2025-10-22 21:39:29 +0200

    Changes

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,14 +1,21 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
+const repoName = 'FinalProject-CryptoTrack';
 
 export default defineConfig({
     plugins: [react(), tailwindcss()],
+
+    base: `/${repoName}/`,
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
         },
     },
+    build: {
+        outDir: 'dist'
+    }
 })
